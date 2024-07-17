a09259554078edfcd56cbe5e3787eec5cc886285
vamage <vamage@vamage.com>
2024-07-17 14:53:45 -0400

    adding SQLc to the builds (#8)
    Adding SQLc to go generate

--- generate.go
+++ generate.go
@@ -1,5 +1,7 @@
-// Purpose: This file is used to generate the api package from the openapi.yaml file.
+// Purpose: This file is used to generate any code for the module
 package main
 
 // This generates the api package from the openapi.yaml file
 //go:generate go run github.com/ogen-go/ogen/cmd/ogen --target api -package api --clean openapi.yaml
+// This generates the user module with sqlc
+//go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc generate -f sql/user/sqlc.yaml
